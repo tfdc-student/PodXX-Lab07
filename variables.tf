cff88f3e288a327fdca6b3fe8e2f74983d1558f8
tfdc-student <101262372+tfdc-student@users.noreply.github.com>
2022-04-13 10:08:54 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  type = string
+  default = "us-east-2"
+}
+
+variable "keyID" {
+  type = string
+}
+
+variable "secretKey" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
